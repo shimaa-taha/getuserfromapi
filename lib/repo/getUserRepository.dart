ae5630fa394a27f3ced39d1ac1dad624fcb189a2
shimaa <shimaataha637@gmail.com>
2025-09-25 19:10:57 +0300

    final

--- lib/repo/getUserRepository.dart
+++ lib/repo/getUserRepository.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../model/getUserModel.dart';
-
+///) retrieves character data from the Rick and Morty API. It makes an HTTP GET request to the API endpoint, decodes the JSON response, extracts the 'results' array, and maps each JSON object to a Results model using fromJson(). If the request fails (non-200 status), it throws an exception. The function returns a Future containing a list of Results objects.
 Future<List<Results>> fetchUsers() async {
   final url = Uri.parse('https://rickandmortyapi.com/api/character');
   final response = await http.get(url);
